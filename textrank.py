3a5e6d107e30f3866b0264c4cea60d2599c182b1
Manoel Vilela <manoel_vilela@engineer.com>
2015-08-29 22:05:51 -0300

    Port Python3, PEP8 fixes and minor coding fixes

--- textrank.py
+++ textrank.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+
 """
-From this paper: http://acl.ldc.upenn.edu/acl2004/emnlp/pdf/Mihalcea.pdf 
+From this paper: http://acl.ldc.upenn.edu/acl2004/emnlp/pdf/Mihalcea.pdf
 
 External dependencies: nltk, numpy, networkx
 
@@ -9,16 +11,18 @@ Based on https://gist.github.com/voidfiles/1646117
 import io
 import nltk
 import itertools
-from operator import itemgetter
 import networkx as nx
 import os
 
-#apply syntactic filters based on POS tags
+
+# apply syntactic filters based on POS tags
 def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
     return [item for item in tagged if item[1] in tags]
 
+
 def normalize(tagged):
     return [(item[0].replace('.', ''), item[1]) for item in tagged]
+
 
 def unique_everseen(iterable, key=None):
     "List unique elements, preserving order. Remember all elements ever seen."
@@ -27,7 +31,7 @@ def unique_everseen(iterable, key=None):
     seen = set()
     seen_add = seen.add
     if key is None:
-        for element in itertools.ifilterfalse(seen.__contains__, iterable):
+        for element in (x for x in iterable if x not in seen):
             seen_add(element)
             yield element
     else:
@@ -37,10 +41,13 @@ def unique_everseen(iterable, key=None):
                 seen_add(k)
                 yield element
 
+
 def lDistance(firstString, secondString):
-    "Function to find the Levenshtein distance between two words/sentences - gotten from http://rosettacode.org/wiki/Levenshtein_distance#Python"
+    """Function to find the Levenshtein distance between two words/sentences
+    - gotten from http://rosettacode.org/wiki/Levenshtein_distance#Python"""
     if len(firstString) > len(secondString):
         firstString, secondString = secondString, firstString
+
     distances = range(len(firstString) + 1)
     for index2, char2 in enumerate(secondString):
         newDistances = [index2 + 1]
@@ -48,17 +55,21 @@ def lDistance(firstString, secondString):
             if char1 == char2:
                 newDistances.append(distances[index1])
             else:
-                newDistances.append(1 + min((distances[index1], distances[index1+1], newDistances[-1])))
+                minime = min((distances[index1],
+                              distances[index1+1],
+                              newDistances[-1]))
+                newDistances.append(1 + minime)
         distances = newDistances
     return distances[-1]
 
+
 def buildGraph(nodes):
     "nodes - list of hashables that represents the nodes of the graph"
-    gr = nx.Graph() #initialize an undirected graph
+    gr = nx.Graph()  # initialize an undirected graph
     gr.add_nodes_from(nodes)
     nodePairs = list(itertools.combinations(nodes, 2))
 
-    #add edges to the graph (weighted by Levenshtein distance)
+    # add edges to the graph (weighted by Levenshtein distance)
     for pair in nodePairs:
         firstString = pair[0]
         secondString = pair[1]
@@ -67,38 +78,47 @@ def buildGraph(nodes):
 
     return gr
 
+
 def extractKeyphrases(text):
-    #tokenize the text using nltk
+    # tokenize the text using nltk
     wordTokens = nltk.word_tokenize(text)
 
-    #assign POS tags to the words in the text
+    # assign POS tags to the words in the text
     tagged = nltk.pos_tag(wordTokens)
     textlist = [x[0] for x in tagged]
-    
     tagged = filter_for_tags(tagged)
     tagged = normalize(tagged)
 
     unique_word_set = unique_everseen([x[0] for x in tagged])
     word_set_list = list(unique_word_set)
 
-   #this will be used to determine adjacent words in order to construct keyphrases with two words
+    # this will be used to determine adjacent words
+    # in order to construct keyphrases with two words
 
     graph = buildGraph(word_set_list)
 
-    #pageRank - initial value of 1.0, error tolerance of 0,0001, 
+    # pageRank - initial value of 1.0, error tolerance of 0,0001
     calculated_page_rank = nx.pagerank(graph, weight='weight')
 
-    #most important words in ascending order of importance
-    keyphrases = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
+    # most important words in ascending order of importance
+    keyphrases = sorted(calculated_page_rank,
+                        key=calculated_page_rank.get,
+                        reverse=True)
 
-    #the number of keyphrases returned will be relative to the size of the text (a third of the number of vertices)
-    aThird = len(word_set_list) / 3
+    # the number of keyphrases returned will be relative
+    # to the size of the text
+    aThird = len(word_set_list) // 3
     keyphrases = keyphrases[0:aThird+1]
 
-    #take keyphrases with multiple words into consideration as done in the paper - if two words are adjacent in the text and are selected as keywords, join them
-    #together
-    modifiedKeyphrases = set([])
-    dealtWith = set([]) #keeps track of individual keywords that have been joined to form a keyphrase
+    # take keyphrases with multiple words
+    # into consideration as done in the paper if two words are adjacent
+    # in the text and are selected as keywords, join them together
+    modifiedKeyphrases = set()
+
+    # keeps track of individual keywords
+    # that have been joined to form a keyphrase
+    dealtWith = set()
+
     i = 0
     j = 1
     while j < len(textlist):
@@ -110,30 +130,32 @@ def extractKeyphrases(text):
             dealtWith.add(firstWord)
             dealtWith.add(secondWord)
         else:
-            if firstWord in keyphrases and firstWord not in dealtWith: 
+            if firstWord in keyphrases and firstWord not in dealtWith:
                 modifiedKeyphrases.add(firstWord)
 
-            #if this is the last word in the text, and it is a keyword,
-            #it definitely has no chance of being a keyphrase at this point    
-            if j == len(textlist)-1 and secondWord in keyphrases and secondWord not in dealtWith:
-                modifiedKeyphrases.add(secondWord)
-        
-        i = i + 1
-        j = j + 1
-        
+            # if this is the last word in the text, and it is a keyword,
+            # it definitely has no chance of being a keyphrase at this point
+            if j == (len(textlist) - 1) and secondWord in keyphrases:
+                if secondWord not in dealtWith:
+                    modifiedKeyphrases.add(secondWord)
+        i += 1
+        j += 1
+
     return modifiedKeyphrases
+
 
 def extractSentences(text):
     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
     sentenceTokens = sent_detector.tokenize(text.strip())
     graph = buildGraph(sentenceTokens)
-
     calculated_page_rank = nx.pagerank(graph, weight='weight')
 
-    #most important sentences in ascending order of importance
-    sentences = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
+    # most important sentences in ascending order of importance
+    sentences = sorted(calculated_page_rank,
+                       key=calculated_page_rank.get,
+                       reverse=True)
 
-    #return a 100 word summary
+    # return a 100 word summary
     summary = ' '.join(sentences)
     summaryWords = summary.split()
     summaryWords = summaryWords[0:101]
@@ -141,28 +163,35 @@ def extractSentences(text):
 
     return summary
 
+
 def writeFiles(summary, keyphrases, fileName):
     "outputs the keyphrases and summaries to appropriate files"
-    print "Generating output to " + 'keywords/' + fileName
+    print("Generating output to " + 'keywords/' + fileName)
     keyphraseFile = io.open('keywords/' + fileName, 'w')
+
     for keyphrase in keyphrases:
         keyphraseFile.write(keyphrase + '\n')
     keyphraseFile.close()
 
-    print "Generating output to " + 'summaries/' + fileName
+    print("Generating output to " + 'summaries/' + fileName)
     summaryFile = io.open('summaries/' + fileName, 'w')
     summaryFile.write(summary)
     summaryFile.close()
 
-    print "-"
+    print("-")
 
 
-#retrieve each of the articles
-articles = os.listdir("articles")
-for article in articles:
-    print 'Reading articles/' + article
-    articleFile = io.open('articles/' + article, 'r')
-    text = articleFile.read()
-    keyphrases = extractKeyphrases(text)
-    summary = extractSentences(text)
-    writeFiles(summary, keyphrases, article)
+def main():
+    # retrieve each of the articles
+    articles = os.listdir("articles")
+    for article in articles:
+        print('Reading articles/' + article)
+        articleFile = io.open('articles/' + article, 'r')
+        text = articleFile.read()
+        keyphrases = extractKeyphrases(text)
+        summary = extractSentences(text)
+        writeFiles(summary, keyphrases, article)
+
+
+if __name__ == '__main__':
+    main()
